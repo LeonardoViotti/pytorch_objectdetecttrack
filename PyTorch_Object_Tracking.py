742bbb1cee19c6150659a37d47cfdefff4885d78
LeonardoViotti <leonardoviotti@gmail.com>
2021-01-11 11:01:53 -0500

    Overall reorganiztion of tracking code.

--- PyTorch_Object_Tracking.py
+++ PyTorch_Object_Tracking.py
@@ -13,6 +13,8 @@ from sort import *
 
 # Other libraraies
 import os, sys, time, datetime, random
+import copy as cp
+
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
@@ -80,6 +82,67 @@ def detect_image(img, img_size = img_size):
     #   - 7 class
     return detections[0]
 
+
+#------------------------------------------------------------------------------
+# Video annotation functions
+
+def detectionToPixel(
+    img, 
+    detection,
+    img_size = img_size):
+    """
+    Parameters
+    ----------
+    img : an array image 
+    detection : a (6,) array, where 4 first elements are detection coordinates, 
+                5th element is track id and 6th is class id
+    img_size : ??
+    """
+    
+    # Variables to convert from detections coordiantes
+    pad_x = max(img.shape[0] - img.shape[1], 0) * (img_size / max(img.shape))
+    pad_y = max(img.shape[1] - img.shape[0], 0) * (img_size / max(img.shape))
+    unpad_h = img_size - pad_y
+    unpad_w = img_size - pad_x
+    
+    # Detection values
+    x1, y1, x2, y2, obj_id, cls_pred = detection
+    
+    box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
+    box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
+    y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
+    x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
+    
+    # Return array with top left corner of detection, box dimentions, tracking id and class
+    return [x1, y1, box_h, box_w, obj_id, cls_pred]
+
+def drawBox(img, 
+            detection,
+            colors = colors):
+    """
+    Parameters
+    ----------
+    img : an array image 
+    detection : 
+    colors :
+    """
+    x1, y1, box_h, box_w, obj_id, cls_pred = detection
+    
+    # Set colors based on detection class and class albels
+    color = colors[int(obj_id) % len(colors)]
+    color = [i * 255 for i in color]
+    cls = classes[int(cls_pred)]
+    
+    # Bounding box
+    cv2.rectangle(img, (x1, y1), (x1+box_w, y1+box_h), color, 2)
+    # Class label rectangle
+    cv2.rectangle(img, (x1, y1-20), (x1+len(cls)*15+10, y1), color, -1)
+    # Class label text
+    cv2.putText(img, cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, .4, (255,255,255), 1)
+
+def drawPoint(img, detection):
+    x1, y1, box_h, box_w, obj_id, cls_pred = detection
+    cv2.circle(img, (x1,y1), 1, (255,0,255), 2)
 #------------------------------------------------------------------------------
 # Video settings
 
@@ -90,61 +153,51 @@ colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 mot_tracker = Sort() 
 vid = cv2.VideoCapture(videopath)
 
-#------------------------------------------------------------------------------
-# Video annotation functions
+# Get initial frame for testing
+ret, frame0 = vid.read()
+frame0 = cv2.cvtColor(frameo, cv2.COLOR_BGR2RGB)
 
-# Draw bbox
-
-# Draw centroid
-
-# Draw trajctorie
+# Detections on initial frame
+pilimg0 = Image.fromarray(frame0)
+detections0 = detect_image(pilimg0)
 
 #------------------------------------------------------------------------------
 # Process video frame by frame
+
+tracked_objects = mot_tracker.update(detections0.cpu())
+
+pix_detecttion_0 = detectionToPixel(frame0, tracked_objects[0])
+
+drawBox(frame0, pix_detecttion_0)
+drawPoint(frame0, pix_detecttion_0)
+
 #while(True):
-for ii in range(40):
-    
+for ii in range(10):   
     # Video frame
-    ret, frame = vid.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    ret, frame_i = vid.read()
+    frame_i = cv2.cvtColor(frame_i, cv2.COLOR_BGR2RGB)
     
     # Detections
-    pilimg = Image.fromarray(frame)
-    detections = detect_image(pilimg)
+    pilimg_i = Image.fromarray(frame_i)
+    detections_i = detect_image(pilimg_i)
     
-    img = np.array(pilimg)
-    pad_x = max(img.shape[0] - img.shape[1], 0) * (img_size / max(img.shape))
-    pad_y = max(img.shape[1] - img.shape[0], 0) * (img_size / max(img.shape))
-    unpad_h = img_size - pad_y
-    unpad_w = img_size - pad_x
-    if detections is not None:
-        tracked_objects = mot_tracker.update(detections.cpu())
+    pix_detecttion_i = detectionToPixel(frame_i, tracked_objects[-1])
+    
+    if detections_i is not None:
+        tracked_objects_i = mot_tracker.update(detections_i.cpu())
         
-        unique_labels = detections[:, -1].cpu().unique()
-        n_cls_preds = len(unique_labels)
-        for x1, y1, x2, y2, obj_id, cls_pred in tracked_objects:
-            box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
-            box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
-            y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
-            x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
-            
-            color = colors[int(obj_id) % len(colors)]
-            color = [i * 255 for i in color]
-            cls = classes[int(cls_pred)]
-            # Bounding box
-            cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 2)
-            # Class label rectangle
-            # cv2.rectangle(frame, (x1, y1-35), (x1+len(cls)*19+60, y1), color, -1)
-            cv2.rectangle(frame, (x1, y1-20), (x1+len(cls)*15+10, y1), color, -1)
-            
-            # Class label text
-            cv2.putText(frame, cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, .4, (255,255,255), 1)
+        for obj_j in tracked_objects_i:
+            pix_detecttion_j = detectionToPixel(frame_i, obj_j)
+            drawBox(frame_i, pix_detecttion_j)
+            # drawPoint(frame, pix_detecttion_j)
+    
     # Show video
-    cv2.imshow("Video", frame)
+    cv2.imshow("Video", frame_i)
     # Break out by pressing 'q' when window is selected
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Make sure there are no open graphics devices
 cv2.destroyAllWindows()
+
 
